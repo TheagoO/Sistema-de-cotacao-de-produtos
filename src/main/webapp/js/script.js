8fc1b84a97b87c88ac7e374af92d0b93f956c088
Thiago Ferreira <theagooo1212@gmail.com>
2021-10-14 13:54:35 -0300

    Tela de cadastro de itens

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,11 @@
+var btn = document.getElementById("menu");
+
+
+btn.addEventListener('click', function(){
+	var div = document.querySelector('.menu-area');
+	
+	if(div.style.display === 'block'){
+		div.style.display = 'none';
+	}
+	
+});
