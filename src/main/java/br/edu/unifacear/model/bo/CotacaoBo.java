e0a7f4e36f0a2892a3a3845bb22f1b16685d0fea
Lucasmtt <lurmtt@gmail.com>
2021-10-29 20:51:34 -0300

    adicionado Bo e Dao no Cotacao

---
+++ src/main/java/br/edu/unifacear/model/bo/CotacaoBo.java
@@ -0,0 +1,62 @@
+package br.edu.unifacear.model.bo;
+
+import java.util.List;
+
+import br.edu.unifacear.model.dao.CotacaoDao;
+import br.edu.unifacear.model.entity.Cotacao;
+
+public class CotacaoBo {
+	
+	public String salvar(Cotacao cotacao) 
+			throws Exception {
+		validarDadosCotacao(cotacao);
+		CotacaoDao cotacaoDao = new CotacaoDao();
+		try {
+			return cotacaoDao.salvar(cotacao);
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+	}
+	
+	public String alterar(Cotacao cotacao) 
+			throws Exception {
+		validarDadosCotacao(cotacao);
+		// exemplo chamando a DAO com a instancia direta do obj
+		try {
+			return new CotacaoDao().alterar(cotacao);
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+	}
+	
+	public String deletar(Cotacao cotacao) 
+			throws Exception {
+		validarDadosCotacao(cotacao);
+		// exemplo chamando a DAO com a instancia direta do obj
+		try {
+			return new CotacaoDao().deletar(cotacao);
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+	}
+	
+	public List<Cotacao> listar(String paramNome) throws Exception {
+		try {
+			return new CotacaoDao().listar(paramNome);
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+	}		
+	
+
+
+	private void validarDadosCotacao(Cotacao cotacao) throws Exception {
+//		if (cotacao.getId() < 0) {
+//			throw new Exception("Id do cotacao n�o pode ser negativo!");
+//		}
+		if (cotacao.getItem().equals("")) {
+			throw new Exception("Nome do cotacão n�o pode ficar em branco!");
+		}
+	}	
+	
+}
---
+++ src/main/java/br/edu/unifacear/model/dao/CotacaoDao.java
@@ -0,0 +1,99 @@
+package br.edu.unifacear.model.dao;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.*;
+
+import javax.persistence.*;
+import javax.persistence.Query;
+
+import br.edu.unifacear.model.entity.*;
+import br.edu.unifacear.model.util.Connect;
+
+import org.hibernate.Criteria;
+import org.hibernate.*;
+import org.hibernate.criterion.Restrictions;
+
+public class CotacaoDao {
+	EntityManager em = Connect.connection();
+
+	public String salvar(Cotacao cotacao) throws Exception {
+		String retorno;
+		// Gravar o Cotacao no BD
+		
+		try {			
+			em.getTransaction().begin();
+			em.persist(cotacao);
+			em.getTransaction().commit();		
+			
+			retorno = "Cotacao Inserido com Sucesso!";			
+		} catch (Exception e) {
+			retorno = e.getMessage();
+			throw new Exception("Erro Gravando Cotacao\n"+e.getMessage());
+		} finally {
+			em.close();
+		}
+		return retorno;		
+	} // salvar
+	
+	public String alterar(Cotacao cotacao) throws Exception {
+		String retorno;
+		// Gravar o Cotacao no BD		
+		try {
+	
+			em.getTransaction().begin();
+			em.merge(cotacao);
+			em.getTransaction().commit();	
+			
+			retorno = "Cotacao Alterado com Sucesso!";			
+		} catch (Exception e) {
+			retorno = e.getMessage();
+			throw new Exception("Erro Alterando Cotacao\n"+e.getMessage());
+		}
+		return retorno;		
+	} // alterar
+
+	public String deletar(Cotacao cotacao) throws Exception {
+		String retorno;
+		// Gravar o Cotacao no BD		
+		try {
+
+
+			Cotacao e = em.find(Cotacao.class, cotacao.getId());
+			em.getTransaction().begin();
+			em.remove(e);
+			
+			retorno = "Cotacao Deletado com Sucesso!";			
+		} catch (Exception e) {
+			retorno = e.getMessage();
+			throw new Exception("Erro Deletando Cotacao\n"+e.getMessage());
+		}
+		return retorno;		
+	} // deletar
+
+	
+	
+	public List<Cotacao> listar(String paramNome) throws Exception{		
+	
+		String cWhere = "";
+		Query q = null;
+
+		if(paramNome.equals("")) {
+			q = em.createQuery("select g from Cotacao g");
+		}
+		else {
+			q = em.createQuery("select g from Cotacao g"
+					+" where nome like :nome");
+			q.setParameter("nome", "%"+paramNome+"%");
+		}
+		
+		return q.getResultList();		
+	} //listar
+	
+	
+	public Cotacao getObjectById(Long id) {
+		
+		return em.find(Cotacao.class, id);
+	}
+} // final da classe CotacaoDao
